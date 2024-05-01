fdbdcf166aab61784349095e94ad5dea91c1be76
Paulo Silva <paulomarcelonds@gmail.com>
2024-05-01 17:57:22 -0300

    usando algumas funções de texto

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+
+st.header("Meu primeiro dashboard online")
+st.text('Texto explicativo')
+st.sidebar.text('texto na sidebar')
+st.markdown('# Texto destacado')
+st.caption('Esse texto serve como legenda para a explicação de alguma coisa')
+
